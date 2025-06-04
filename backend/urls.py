d04d82f48d1b6abc615916eec6503111c5632f2f
jovenlab <jolabsjunior@gmail.com>
2025-06-04 12:41:45 +0800

    added home route

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Jose Rizal Chatbot Backend is running!")
 
 urlpatterns = [
+    path("", index),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
